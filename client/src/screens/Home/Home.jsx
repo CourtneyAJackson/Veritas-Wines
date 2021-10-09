776c568141ff5744710471c0c4d8f50bd50a39e9
Courtney Jackson <courtneyjackson@Courtneys-Air.lan>
2021-10-09 16:19:40 -0700

    added icons for material ui

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,8 +1,19 @@
-import 
+import Button from '@mui/material/Button'
 
 function Home() {
   return (
     <div>
+      <Button
+        href='/wines'
+        style={{
+          fontSize: 20
+        }}
+        variant="contained"
+        color="secondary"
+        size='large'
+      >
+        Explore Our Collection
+      </Button>
       <h1>home page</h1>
 
       <button>Explore our collection</button>
