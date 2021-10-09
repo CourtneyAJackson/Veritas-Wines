356033b75f1593bee943569bcf250d766b566aee
Courtney Jackson <courtneyjackson@Courtneys-Air.lan>
2021-10-09 15:58:03 -0700

    created css folders for screens and working on styling for collecting page

--- client/src/screens/Wines/Wines.jsx
+++ client/src/screens/Wines/Wines.jsx
@@ -1,37 +1,43 @@
+import './Wines.css'
 import { Link } from 'react-router-dom'
 import StarRatings from 'react-star-ratings'
-import './Wines.css'
+
 
 
 function Wines(props) {
- 
+
   return (
-    <>
-    <h1>Our Collection</h1>
-    <div className='card-container'>
-     
-     
-      { 
-        props.wines.map(wine => (
-          <div className="wine-card">
-            <img className='wine-img' src={wine.img} alt='Wine Bottle' />
-            <p>{wine.name}</p>
-            <p>{wine.year}</p>
-            {wine.ratings.map((rating) => (
-                 <StarRatings
-                 rating={Number(rating.rank)}
-                 starDimension="40px"
-                 starSpacing="15px"
-               />
-            ))}
-            <button><Link to= {`wine-details/${wine.id}/new`}>Add a </Link></button>
-            <button><Link to= {`wine-details/${wine.id}`}>Details</Link></button>
-          </div>
-        ))
+    <div className= 'wine-collection'>
+      <h1>Our Collection</h1>
+      <div className='card-container'>
+
+
+        {
+          props.wines.map(wine => (
+            <div className="wine-card">
+              <img className='wine-img' src={wine.img} alt='Wine Bottle' />
+              <div className='name-year-title'>
+                <p>{wine.name}</p>
+                <p>{wine.year}</p>
+              </div>
+              {wine.ratings.map((rating) => (
+                <StarRatings
+                  rating={Number(rating.rank)}
+                  starDimension="40px"
+                  starSpacing="15px"
+                />
+              ))}
+              <div className='detail-rating-buttons'>
+                <button className='add-rating-collection-button'><Link to={`wine-details/${wine.id}/new`}>Add a </Link></button>
+               
+                <button className='detail-collection-button'><Link to={`wine-details/${wine.id}`}>Details</Link></button>
+               </div>
+            </div>
+          ))
         }
       </div>
-      </>
-   
+    </div>
+
   )
 }
 
