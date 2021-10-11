807157a51edc3f6cf93d82db53f087d0664b5549
Courtney Jackson <courtneyjackson@Courtneys-Air.lan>
2021-10-11 00:29:12 -0700

    more styling

--- client/src/components/layouts/Layout.jsx
+++ client/src/components/layouts/Layout.jsx
@@ -13,27 +13,25 @@ function Layouts(props) {
         <img className="nav-logo" src="https://lh3.googleusercontent.com/pxh0Tw16-XwE7RbbDtyXRMUa00FvbjxVrv2LGRMqHxg00xXIQz-rRJiUzviBiiBCRcUljvWHJY9xv0WoYLQPE611QSfNRKx7SDQiAcCfWO2357K6hGwmXWpboNT8eNL6YUuzR9SF=s200-p-k"
           alt="logo"
         />
-        <header className="nav-bar">
-          <div className='nav-item'>
-            <Link to='/wines'>Our Collection</Link>
-            {
-              props.currentUser ?
-                (<>
-                  <article>Welcome {props.currentUser.username}!</article>
-                  <button onClick={props.handleLogout}>Sign Out</button>
-                </>) : (<>
-                  <Link to='/logIn'>LogIn</Link>
-                  <Link to='/register'>Register</Link>
-                </>)
+        <div className='nav-links'>
+             <Link className='collection' to='/wines'>Our Collection</Link>
+              {
+                props.currentUser ?
+                  (<>
+                    <article>Welcome {props.currentUser.username}!</article>
+                    <button className='signout' onClick={props.handleLogout}>Sign Out</button>
+                  </>) : (<>
+                    <Link className='login' to='/logIn'>LogIn</Link>
+                    <Link className='register' to='/register'>Register</Link>
+                  </>)
 
-            }
-            {
-              props.currentUser && <>
-                <Link to='/wines'>Our Collection</Link>
-              </>
-            }
-          </div>
-        </header>
+              }
+              {
+                props.currentUser && <>
+                  <Link classNames='wine-collection' to='/wines'>Our Collection</Link>
+                </>
+              }
+        </div>
       </div>
       {props.children}
       <Footer />
