52b80b15f1e1a278ba743ab3790230eadde3b365
Jackson <courtney.jackson@accenture.com>
2023-03-28 13:05:50 -0700

    track logic added

--- client/src/services/wines.js
+++ client/src/services/wines.js
@@ -9,3 +9,13 @@ export const getOneWine = async (id) => {
   const resp = await api.get(`/wines/${id}`);
   return resp.data;
 };
+
+analytics.track({
+  userId:'f4ca124298',
+  event: 'Bookmarked Article',
+  properties: {
+    title: 'Snow Fall',
+    subtitle: 'The Avalanche at Tunnel Creek',
+    author: 'John Branch'
+  }
+});
